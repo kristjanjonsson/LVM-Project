4a5b73ff596842bd338a1effedd8967adffd1265
Fang Qiao <aznranndydrakdi@gmail.com>
2016-06-08 09:21:00 -0700

    Gibbs with mixture.

--- BMFMatlab/demo.m
+++ BMFMatlab/demo.m
@@ -1,11 +1,11 @@
-train_vec = csvread('../data/ml_100k_train.csv')
+train_vec = csvread('../data/ml_100k_train.csv');
 probe_vec = csvread('../data/ml_100k_test.csv');
 
 restart=1;
 fprintf(1,'Running Probabilistic Matrix Factorization (PMF) \n');
 pmf
 
-%restart=1;
-%fprintf(1,'\nRunning Bayesian PMF\n');
-%bayespmf
+restart=1;
+fprintf(1,'\nRunning Bayesian PMF\n');
+bayespmf
 
